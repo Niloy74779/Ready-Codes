0d216c63bd69f321d31a938a3709388e0d57a75e
Niloy74779 <mdniloy74779@gmail.com>
2021-03-15 20:10:28 +0600

    niloy

---
+++ Animated Navbar/app.js
@@ -0,0 +1,18 @@
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () =>{
+    container.classList.toggle("active")
+})
+
+
+
+
+
+
+
+
+
+
+
+
